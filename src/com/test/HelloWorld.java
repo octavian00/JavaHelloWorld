da14ac560fe0be861c8d3e5247538af2d764b27c
octavian <octavian.vilceanu@ligaac.ro>
2019-05-27 19:23:43 +0300

    secondCommit

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -1,5 +1,8 @@
+
+package  com.test;
 public class HelloWorld {
     public static void main(String argv[]){
         System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
 }
